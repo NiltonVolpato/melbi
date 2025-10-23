b0072c320022d3655aac2ec727e445135956d479
Nilton Volpato <nilton@volpa.to>
2025-10-23 05:18:59 -0700

    Refactor parser_debug to improve error handling and integrate analyzer functionality

--- core/src/bin/parser_debug.rs
+++ core/src/bin/parser_debug.rs
@@ -1,18 +1,25 @@
 use bumpalo::Bump;
-use melbi_core::parser;
+use melbi_core::{analyzer, parser, types::manager::TypeManager};
 use std::env;
 
-fn main() {
+fn main() -> std::io::Result<()> {
     let args: Vec<String> = env::args().collect();
     let arena = Bump::new();
-    let parsed = parser::parse(&arena, &args[1]);
-    match parsed {
-        Err(e) => {
-            eprintln!("Error:\n{}", e);
-            return;
-        }
-        Ok(ast) => {
-            println!("Parsed AST:\n{:#?}", ast.expr);
-        }
-    }
+    let type_manager = TypeManager::new(&arena);
+
+    let result = parser::parse(&arena, &args[1]);
+    let Ok(ast) = result else {
+        eprintln!("Parse Error:\n{}", result.unwrap_err());
+        return Ok(());
+    };
+    println!("Parsed AST:\n{:#?}", ast.expr);
+
+    let result = analyzer::analyze(type_manager, &arena, &ast);
+    let Ok(expr) = result else {
+        // Print the error using miette's fancy output, but don't exit
+        eprintln!("{:?}", result.unwrap_err());
+        return Ok(());
+    };
+    println!("Typed Expression:\n{:#?}", expr);
+    Ok(())
 }
